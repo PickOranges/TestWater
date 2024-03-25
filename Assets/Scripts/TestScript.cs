abc2a2caf458e8bd0c5701649fb377efee7d1433
chengzimdl@gmail.com <chengzimdl@gmail.com>
2024-03-25 16:39:21 +0100

    passed necessary settings/params but nothing rendered.

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
@@ -8,7 +9,7 @@ public class TestScript : MonoBehaviour
 {
     public ComputeShader waterFFTShader;
     public Shader waterRenderingShader;
-    Camera _camera;
+    Camera _camera;  // ??? never read
     RenderTexture _target;
     Mesh mesh;
     Material material;
@@ -17,6 +18,8 @@ public class TestScript : MonoBehaviour
     Vector3[] normals;
     float g = 9.81f;
     ComputeBuffer spectrumParamsBuffer;
+
+    private int N, logN, threadGroupsX, threadGroupsY;
 
     public struct SpectrumSettings
     {
@@ -28,9 +31,38 @@ public class TestScript : MonoBehaviour
         public float peakOmega;
         public float gamma;
         public float shortWavesFade;
-        public float windSpeed; // new
+
+        // new
+        public float windSpeed; 
     }
     SpectrumSettings[] spectrums = new SpectrumSettings[8];
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    [Header("Spectrum Settings")]
+    [Range(0, 100000)]
+    public int seed = 0;
+
+    [Range(0.0f, 0.1f)]
+    public float lowCutoff = 0.0001f;
+
+    [Range(0.1f, 9000.0f)]
+    public float highCutoff = 9000.0f;
+
+    [Range(2.0f, 20.0f)]
+    public float depth = 20.0f;
+
+    [Range(0.0f, 200.0f)]
+    public float repeatTime = 200.0f;
+
+    [Range(0.0f, 5.0f)]
+    public float speed = 1.0f;
+
+    public Vector2 lambda = new Vector2(1.0f, 1.0f);
+
+    [Range(0.0f, 10.0f)]
+    public float displacementDepthFalloff = 1.0f;
+
+    public bool updateSpectrum = false;
 
     [System.Serializable]
     public struct DisplaySpectrumSettings
@@ -65,17 +97,32 @@ public class TestScript : MonoBehaviour
     public DisplaySpectrumSettings spectrum6;
     [SerializeField]
     public DisplaySpectrumSettings spectrum7;
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
     private void Start()
     {
-        InitRenderTexture();
+        N = 1024;
+        logN = (int)Mathf.Log(N, 2.0f);
+        threadGroupsX = Mathf.CeilToInt(N / 8.0f);
+        threadGroupsY = Mathf.CeilToInt(N / 8.0f);
 
+
+        // 1. Create Textures
+        //_target = CreateRenderTex(N, N, 4, RenderTextureFormat.ARGBHalf, true);  
+        _target = CreateRenderTex(N, N, 4, RenderTextureFormat.ARGB32, true);
+
+        // 2. Set data
         waterFFTShader.SetTexture(0, "InitSpectrumTexture", _target);
         _camera = Camera.main;
         spectrumParamsBuffer = new ComputeBuffer(8, 9*sizeof(float));
         SetSpectrumBuffers();
+        SetFFTUniforms();
 
+        // 3. Dispatch
+        waterFFTShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+
+        // 4. Render Pipeline
         //CreatePlane();
         //CreateMaterial();
     }
@@ -90,12 +137,23 @@ public class TestScript : MonoBehaviour
             _target.Create();
         }
     }
+
+    RenderTexture CreateRenderTex(int width, int height, int depth, RenderTextureFormat format, bool useMips)
+    {
+        RenderTexture rt = new RenderTexture(width, height, 0, format, RenderTextureReadWrite.Linear);
+        rt.dimension = UnityEngine.Rendering.TextureDimension.Tex2DArray; 
+        rt.filterMode = FilterMode.Bilinear;
+        rt.wrapMode = TextureWrapMode.Repeat;
+        rt.enableRandomWrite = true;
+        rt.volumeDepth = depth; // Number of elements in a texture array(Read Only), i.e. #Tex in this array.
+        rt.useMipMap = useMips;
+        rt.autoGenerateMips = false;
+        rt.anisoLevel = 16;
+        rt.Create();
+        return rt;
+    }
     private void Update()
     {
-        int groupX = _target.width / 8;
-        int groupY=_target.height / 8;
-        waterFFTShader.Dispatch(0,groupX,groupY,1);
-
         RenderPipelineManager.endCameraRendering += OnEndCameraRendering;
     }
    
@@ -214,5 +272,31 @@ public class TestScript : MonoBehaviour
 
         spectrumParamsBuffer.SetData(spectrums);
         waterFFTShader.SetBuffer(0, "Sps", spectrumParamsBuffer);
+    }
+
+    void SetFFTUniforms()
+    {
+        waterFFTShader.SetVector("L", lambda);     
+        //waterFFTShader.SetFloat("_FrameTime", Time.time * speed);
+        //waterFFTShader.SetFloat("_DeltaTime", Time.deltaTime);
+        //waterFFTShader.SetFloat("_RepeatTime", repeatTime);
+        waterFFTShader.SetInt("N", N);
+        waterFFTShader.SetFloat("D", depth);
+        waterFFTShader.SetFloat("_LowCutoff", lowCutoff);
+        waterFFTShader.SetFloat("_HighCutoff", highCutoff);
+
+
+        //waterFFTShader.SetInt("_LengthScale0", lengthScale1);
+        //waterFFTShader.SetInt("_LengthScale1", lengthScale2);
+        //waterFFTShader.SetInt("_LengthScale2", lengthScale3);
+        //waterFFTShader.SetInt("_LengthScale3", lengthScale4);
+        //waterFFTShader.SetFloat("_NormalStrength", normalStrength);
+
+
+        //waterFFTShader.SetFloat("_FoamThreshold", foamThreshold);
+        //waterFFTShader.SetFloat("_FoamBias", foamBias);
+        //waterFFTShader.SetFloat("_FoamDecayRate", foamDecayRate);
+        //waterFFTShader.SetFloat("_FoamThreshold", foamThreshold);
+        //waterFFTShader.SetFloat("_FoamAdd", foamAdd);
     }
 }
